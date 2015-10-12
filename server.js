046f50c2992fa9ad232a287fb366b47b83c9fa20
Evmap <xdima24@gmail.com>
2015-10-12 12:08:50 +0800

    index.jade fix

--- server.js
+++ server.js
@@ -95,8 +95,12 @@ app.get('/edit/:id', function(req, res) {
         }
     });
 });
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+
+var port = process.env.PORT || 5000;
+var host = process.env.HOST || "127.0.0.1";
+
+
+var server = http.createServer(app).listen(port, host, function() {
+    console.log("Сервер стартанул на %s:%d",
+                host, port);
 });
